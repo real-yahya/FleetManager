722bab0a6e75fbf6f6471be7a5d1689725686422
Yahya <ym196@student.le.ac.uk>
2025-09-20 19:57:17 +0100

    Removed Search button in the Search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -6,11 +6,8 @@ const SearchBar = ({searchValue, setSearchValue, onSuccess}) => {
     <div className='bg-white border border-gray-200 py-2.5 px-6 shadow-sm rounded-xl text-lg'>
       <form onSubmit={onSuccess}>
         <div className='flex gap-2'>
-            <input value={searchValue} onChange={(e)=>{setSearchValue(e.target.value)}} className='flex-1 border-none outline-none cursor-pointer focus:placeholder-transparent' type="text" placeholder="'SD69 LHR','AL25 DHM' or 'BC24 KDM' "/>
+            <input value={searchValue} onChange={(e)=>{setSearchValue(e.target.value)}} className='flex-1 border-none outline-none cursor-pointer focus:placeholder-transparent' type="text" placeholder="'SD69 LHR', 'AL25 DHM' or 'BC24 KDM' "/>
             
-            <button className='cursor-pointer hover:text-gray-800 items-center' type='submit'>
-              <FaSearch className='block w-5 h-5'/>
-            </button>
         </div>
       </form>
     </div>
